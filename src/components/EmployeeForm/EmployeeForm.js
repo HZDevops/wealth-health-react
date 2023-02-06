98eb0605bf50d0dda43771af79090227ca4f9ee4
Zoubeiri <housna.zoubeiri@gmail.com>
2023-02-06 11:06:56 +0100

    Replace Select react with select html

--- src/components/EmployeeForm/EmployeeForm.js
+++ src/components/EmployeeForm/EmployeeForm.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useStore } from "react-redux";
-import Select from "react-select";
 import { states, departments } from "../../data/options";
 import { employeeAdded } from "../../actions/employee";
 import { Modal } from "@hzdevops/modal-react-library/dist";
@@ -12,29 +11,16 @@ function EmployeeForm() {
   //Modal state initialisation
   const [isOpen, setModal] = useState(false);
 
-  const [formData, setAddFormData] = useState({
-    firstName: "",
-    lastName: "",
-    startDate: "",
-    department: "",
-    birthDate: "",
-    street: "",
-    city: "",
-    state: "",
-    zipCode: "",
-  });
-
-  const handleAddFormChange = (e) => {
-    e.preventDefault();
-
-    const fieldName = e.target.getAttribute("name");
-    const fieldValue = e.target.value;
-
-    const newFormData = { ...formData };
-    newFormData[fieldName] = fieldValue;
-
-    setAddFormData(newFormData);
-  };
+  //Form state initialisation
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [birthDate, setBirthDate] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("Alabama");
+  const [zipCode, setZipCode] = useState("");
+  const [department, setDepartment] = useState("Sales");
 
   /**
    * Close modal
@@ -43,7 +29,22 @@ function EmployeeForm() {
   const closeModal = () => {
     setModal(false);
   };
-
+  /** 
+   * Parse the date into a string with a specific format
+   * @param {Object} date - date selected on the Create Employee form
+   * @param {String} option - option related the date field form :birthDate or startDate
+   *
+  const parseDateValue = (date, option) => {
+    const regexDate = new RegExp(/(^..........)/);
+    const dateParsed = date.toISOString().match(regexDate)[0];
+    if (option === "birthDate") {
+      setBirthDateValue(dateParsed);
+      setBirthDate(date);
+    } else if (option === "startDate") {
+      setStartDateValue(dateParsed);
+      setStartDate(date);
+    }
+  };*/
   /**
    * Get the data from the form stored in the state locale,
    * check if the form is valid -> formValidation(),
@@ -52,19 +53,18 @@ function EmployeeForm() {
    */
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    const newEmployee = {
-      firstName: formData.firstName,
-      lastName: formData.lastName,
-      startDate: formData.startDate,
-      department: formData.department,
-      birthDate: formData.birthDate,
-      street: formData.street,
-      city: formData.city,
-      state: formData.state,
-      zipCode: formData.zipCode,
+    const formData = {
+      firstName: firstName,
+      lastName: lastName,
+      birthDate: birthDate,
+      startDate: startDate,
+      street: street,
+      city: city,
+      state: state,
+      zipCode: zipCode,
+      department: department,
     };
-    store.dispatch(employeeAdded(newEmployee));
+    store.dispatch(employeeAdded(formData));
     setModal(true);
 
     const form = document.getElementById("form-employee");
@@ -80,8 +80,8 @@ function EmployeeForm() {
           <input
             className="form-input"
             type="text"
-            name="firstName"
-            onChange={handleAddFormChange}
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
             required
           />
         </div>
@@ -90,8 +90,8 @@ function EmployeeForm() {
           <input
             className="form-input"
             type="text"
-            name="lastName"
-            onChange={handleAddFormChange}
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
             required
           />
         </div>
@@ -101,7 +101,7 @@ function EmployeeForm() {
             className="formInput"
             type="date"
             name="birthDate"
-            onChange={handleAddFormChange}
+            onChange={(e) => setBirthDate(e.target.value)}
             required
           />
         </div>
@@ -111,7 +111,7 @@ function EmployeeForm() {
             className="formInput"
             type="date"
             name="startDate"
-            onChange={handleAddFormChange}
+            onChange={(e) => setStartDate(e.target.value)}
             required
           />
         </div>
@@ -121,8 +121,8 @@ function EmployeeForm() {
           <input
             className="form-input"
             type="text"
-            name="street"
-            onChange={handleAddFormChange}
+            value={street}
+            onChange={(e) => setStreet(e.target.value)}
             required
           />
         </div>
@@ -131,40 +131,60 @@ function EmployeeForm() {
           <input
             className="form-input"
             type="text"
-            name="city"
-            onChange={handleAddFormChange}
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
             required
           />
         </div>
         <div className="form-element">
           <label htmlFor="">State</label>
-          <Select
-            defaultValue={states[0]}
-            options={states}
-            onChange={handleAddFormChange}
-          />
+          <select
+            className="form-select"
+            name="state"
+            onChange={(e) => setState(e.target.value)}
+            required
+          >
+            <option value=""></option>
+            {states.map((state, index) => {
+              return (
+                <option key={index} value={state.label}>
+                  {state.label}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <div className="form-element">
           <label htmlFor="">Zip code</label>
           <input
             className="form-input"
             type="text"
-            name="zipCode"
+            value={zipCode}
             minLength="5"
             maxLength="5"
             pattern="[0-9.]+"
             title="Zip code must be 5 digits"
-            onChange={handleAddFormChange}
+            onChange={(e) => setZipCode(e.target.value)}
             required
           />
         </div>
         <div className="form-element">
           <label htmlFor="">Department</label>
-          <Select
-            defaultValue={departments[0]}
-            options={departments}
-            onChange={handleAddFormChange}
-          />
+          <select
+            className="form-select"
+            name="department"
+            onChange={(e) => setDepartment(e.target.value)}
+            required
+          >
+            <option value=""></option>
+            {departments.map((department, index) => {
+              return (
+                <option key={index} value={department.label}>
+                  {department.label}
+                </option>
+              );
+            })}
+          </select>
         </div>
         <button className="save-button" onClick={handleSubmit}>
           Sign In
